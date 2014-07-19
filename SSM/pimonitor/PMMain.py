6cfb010ccfeb912b564509956b2e52ba458064bc
Adam <adamyoung600@gmail.com>
2014-07-18 21:37:18 -0400

    Added in Modified PiMonitor files

---
+++ SSM/pimonitor/PMMain.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on 29-03-2013
+
+@author: citan
+'''
+
+import array
+import os
+import os.path
+import time
+import cPickle as pickle
+
+from SSM.pimonitor.PM import PM
+from SSM.pimonitor.PMConnection import PMConnection
+from SSM.pimonitor.PMPacket import PMPacket
+from SSM.pimonitor.PMParameter import PMParameter
+from SSM.pimonitor.PMUtils import PMUtils
+from SSM.pimonitor.PMXmlParser import PMXmlParser
+
+
+if __name__ == '__main__':
+    
+    parser = PMXmlParser()
+
+    supported_parameters = []
+	
+    defined_parameters = parser.parse("logger_METRIC_EN_v131.xml")	
+
+    connection = PMConnection()
+
+    while True:
+        try:
+            
+            
+            connection.open()
+            #Query ecu/tcu to see which parameters are supported.
+			ecu_packet = connection.init(1)
+            tcu_packet = connection.init(2)
+						
+            if ecu_packet == None or tcu_packet == None:
+                print "Can't get initial data."
+                continue;
+
+            #Match the defined parameters against which ones are in teh ecu/tcu supported parameters
+            for p in defined_parameters:
+                if (p.get_target() & 0x1 == 0x1) and p.is_supported(ecu_packet.to_bytes()[5:]):
+                    if not filter(lambda x: x.get_id() == p.get_id(), supported_parameters):
+                        supported_parameters.append(p)
+
+            for p in defined_parameters:
+                if ((p.get_target() & 0x2 == 0x2) or (p.get_target() & 0x1 == 0x1)) and p.is_supported(tcu_packet.to_bytes()[5:]):
+                    if not filter(lambda x: x.get_id() == p.get_id(), supported_parameters):
+                        supported_parameters.append(p)
+
+            for p in defined_parameters:
+                p_deps = p.get_dependencies();
+                if not p_deps:
+                    continue
+
+                deps_found = ()
+                for dep in p_deps:
+                    deps_found = filter(lambda x: x.get_id() == dep, supported_parameters)
+                    if not deps_found:
+                        break
+
+                    if len(deps_found) > 1:
+                        raise Exception('duplicated dependencies', deps_found)
+									
+                    p.add_parameter(deps_found[0])
+
+                if deps_found:
+                    supported_parameters.append(p)
+				
+            # each ID must be in a form P01 - first letter, then a number
+			supported_parameters.sort(key=lambda p: int(p.get_id()[1:]), reverse=False)
+
+            print "==================================="
+            print "Supported Parameters Below:"
+            print "==================================="
+            #Print out the supported parameters
+            for p in supported_parameters:
+                print p.to_string()
+
+
+
+
+
+
+
+	# 		for p in supported_parameters:
+	# 			window = PMSingleWindow(p)
+	# 			screen.add_window(window)
+    #
+	# 		while True:
+	# 			window = screen.get_window()
+	# 			param = window.get_parameter()
+	# 			parameters = param.get_parameters()
+	# 			if parameters:
+	# 				packets = connection.read_parameters(parameters)
+	# 				window.set_packets(packets)
+	# 			else:
+	# 				packet = connection.read_parameter(param)
+	# 				window.set_packets([packet])
+	#
+	# 			#ecu_response_packets = connection.read_parameters(ecu_params)
+	# 			#tcu_response_packets = connection.read_parameters(tcu_params)
+    #
+	# 			#param_no = 0
+	# 			#for ecu_packet in ecu_response_packets:
+	# 			#	param = ecu_params[param_no]
+	# 			#	window.set_value(param, ecu_packet)
+	# 			#	param_no += 1
+    #
+	# 			#param_no = 0
+	# 			#for tcu_packet in tcu_response_packets:
+	# 			#	param = tcu_params[param_no]
+	# 			#	window.set_value(param, tcu_packet)
+	# 			#	param_no += 1
+    #
+	# 			screen.render()
+    #
+	# 	except IOError as e:
+	# 		PM.log('I/O error: {0} {1}'.format(e.errno, e.strerror), log_id)
+	# 		if connection != None:
+	# 			connection.close()
+	# 			time.sleep(3)
+	# 		continue
+    #
+	# screen.close()
---
+++ SSM/pimonitor/PMXmlParser.py
@@ -0,0 +1,117 @@
+'''
+Created on 29-03-2013
+
+@author: citan
+'''
+
+import xml.sax
+import os.path
+
+from PM import PM
+from PMParameter import PMParameter
+
+# TODO: dependencies
+# TODO: ecuparams
+
+class PMXmlParser(xml.sax.ContentHandler):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        xml.sax.ContentHandler.__init__(self)
+        
+    def parse(self, file_name):
+        self._parameters = set()
+        self._parameter = None
+        self._element_no = 0
+        
+        self._message = "Parsing XML data"
+        source = open(os.path.join("SSM\data", file_name))
+        xml.sax.parse(source, self)
+        
+        return self._parameters
+
+    """
+    Override to make sure we parse the romraider xml properly
+    """
+    def startElement(self, name, attrs):
+        if name == "parameter":
+            # set optional arguments
+            byte_index = "none"
+            bit_index = "none"
+
+            for (k,v) in attrs.items():
+                if k == "id":
+                    pid = v
+                if k == "name":
+                    name = v
+                if k == "desc":
+                    desc = v
+                if k == "ecubyteindex":
+                    byte_index = int(v)
+                if k == "ecubit":
+                    bit_index = int(v)
+                if k == "target":
+                    target = int(v)
+
+            self._parameter = PMParameter(pid, name, desc, byte_index, bit_index, target)
+                          
+        if name == "address":
+            self._addrlen = 1
+            for (k,v) in attrs.items():
+                if k == "length":
+                    self._addrlen = int(v)
+                    
+        if name == "depends":
+            self._addrlen = 0
+
+        if name == "ref":
+            for (k,v) in attrs.items():
+                if k == "parameter":
+                    self._parameter.add_dependency(v)
+            
+        if name == "conversion" and self._parameter != None:
+            for (k,v) in attrs.items():
+                if k == "units":
+                    units = v
+                if k == "expr":
+                    expr = v
+                if k == "format":
+                    value_format = v
+                    
+            if self._parameter != None:
+                self._parameter.add_conversion([units, expr, value_format])
+        
+        self._name = name
+
+    """
+    This is an overirde of the content handler class.  Needed to parse the rom raider xml file
+    """
+    def characters(self, content):
+        if len(content.strip()) > 0 and self._name == "address" and self._parameter != None:
+            self._parameter.set_address(int(content, 16), self._addrlen)
+
+    """
+    Override to make sure we parse the romraider xml properly
+    """
+    def endElement(self, name):
+        if name == "parameter":
+            self._parameters.add(self._parameter)
+            self._parameter = None
+            self._addrlen = None
+             
+        if name == "address":
+            self._addrlen = 0
+
+        if name == "depends":
+            pass
+
+        self._name = ""
+        
+        self._element_no += 1
+
