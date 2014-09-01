a89ee8c9caace4996d81b102339714b425ed3368
Adam <adamyoung600@gmail.com>
2014-09-01 11:49:09 -0400

    Change to gear indicator test

--- TestGearDisplay.py
+++ TestGearDisplay.py
@@ -10,6 +10,6 @@ if __name__=="__main__":
     while 1:
         testGearDisplay.DisplayNeutral()
         time.sleep(1)
-        for i in range(6):
-            testGearDisplay.DisplayGear(i)
+        for i in range(5):
+            testGearDisplay.DisplayGear(i+1)
             time.sleep(1)
