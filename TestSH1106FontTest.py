6a40b81d47375c66d0c086584a919cdd9a2d0e65
Adam <adamyoung600@gmail.com>
2014-09-04 22:14:41 -0400

    First pass of SH1106 font

--- TestSH1106FontTest.py
+++ TestSH1106FontTest.py
@@ -4,7 +4,9 @@ from Hardware.SH1106.SH1106FontLib import *
 
 i2cConfig()
 lcd = SH1106LCD()
+time.sleep(1)
 lcd.setCursorPosition(0, 0)
+time.sleep(1)
 myFont = font2
 for i in myFont:
     lcd.sendDataByte(i)
