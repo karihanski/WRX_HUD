a3f00ab4bcd69a54105a33fa2cd1c8418b0fd738
Adam <adamyoung600@gmail.com>
2014-09-06 18:57:50 -0400

    Implemented the ability to display an inverted string (as if highlighted).

--- TestSH1106FontTest.py
+++ TestSH1106FontTest.py
@@ -7,6 +7,5 @@ lcd = SH1106LCD()
 lcd.displayInvertedString("Test inversion", 0, 0)
 lcd.displayString("In between", 1, 0)
 lcd.displayInvertedString("Second Test", 2, 0)
-lcd.clearScreen()
 lcd.displayString("This is to test the text wrapping ability of my super awesome LCD screen", 5, 0)
 
