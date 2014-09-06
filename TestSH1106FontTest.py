1d53e3e0438e5278861ad734d96fd5fe3cd0e51b
Adam <adamyoung600@gmail.com>
2014-09-06 17:27:52 -0400

    Fixed an offset problem in SH1106.setCursorPostion() Adding text wrapping to SH1106.displayString()

--- TestSH1106FontTest.py
+++ TestSH1106FontTest.py
@@ -8,5 +8,5 @@ lcd.displayString("This is a test!", 0, 0)
 lcd.displayString("Skip a row, please", 2, 10)
 lcd.displayString("@#$%^&*()-_=+,.<>", 4,0)
 lcd.clearScreen()
-lcd.displayString("This is to test the text wrapping ability of my super awesome LCD screen")
+lcd.displayString("This is to test the text wrapping ability of my super awesome LCD screen", 5, 0)
 
