2a0130c83fc3ea4d53c8052f775c45599720812f
Adam <adamyoung600@gmail.com>
2014-09-09 19:18:07 -0400

    Added software debounce to the keypad test.

--- TestCase/TestKeypad.py
+++ TestCase/TestKeypad.py
@@ -3,17 +3,39 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+
+upTimeStamp = 0
+downTimeStamp = 0
+backTimeStamp = 0
+setTimeStamp = 0
+
 def upCallback(channel):
-    print("Up")
+    global upTimeStamp
+    tempTime = time.time()
+    if (tempTime - upTimeStamp) >= 0.2:
+        print("Up")
+        upTimeStamp = tempTime
 
 def downCallback(channel):
-    print("Down")
+    global downTimeStamp
+    tempTime = time.time()
+    if (tempTime - downTimeStamp) >= 0.2:
+        print("Down")
+        downTimeStamp = tempTime
 
 def setCallback(channel):
-    print("Set")
+    global setTimeStamp
+    tempTime = time.time()
+    if (tempTime - setTimeStamp) >= 0.2:
+        print("Set")
+        setTimeStamp = tempTime
 
 def backCallback(channel):
-    print("Back")
+    global backTimeStamp
+    tempTime = time.time()
+    if (tempTime - backTimeStamp) >= 0.2:
+        print("Back")
+        backTimeStamp = tempTime
 
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
