a7e4db12931e8a5845898e29482b3a50c78770a4
Adam <adamyoung600@gmail.com>
2014-09-09 22:35:51 -0400

    Added test for buttons and menuing system.

--- MenuSystem/MainMenuContext.py
+++ MenuSystem/MainMenuContext.py
@@ -8,14 +8,15 @@ class MainMenuContext(MenuContext):
         super(MainMenuContext, self).__init__(inManager, inLcd)
         self.title = "Main"         #Title to display at the top of the display when this menu is active.
         self.entries = ["SSM Parameters", "Peak Boost", "Test1", "Test2"]           #Holds a reference to all the possible entries in the menu
+        self.isSet = [False, False, False, False]
 
     """
     Used to display the menu data on the LCD
     """
     def initDisplay(self):
         self.lcd.displayString(self.title, 0, 0)
-        self.lcd.displayString("-----------------------------------", 1, 0)
-        for i in range(0, len(self.entries)-1):
+        self.lcd.displayString("--------------------------------", 1, 0)
+        for i in range(0, len(self.entries)):
             self.lcd.displayString(self.entries[i], i+2, 17)
         self.lcd.displayString(">", 2, 5)
 
@@ -31,18 +32,25 @@ class MainMenuContext(MenuContext):
     """
     def onUp(self):
         if self.currentEntry > 0:
+            print "Up"
             self.lastEntry = self.currentEntry
             self.currentEntry -= 1
             self.updateDisplay()
 
     def onDown(self):
         if self.currentEntry < len(self.entries)-1:
+            print "Down"
             self.lastEntry = self.currentEntry
             self.currentEntry += 1
             self.updateDisplay()
 
     def onSet(self):
-        self.lcd.displayString("*", self.currentEntry+2, 11)
-
+        print "Set"
+        if not self.isSet(self.currentEntry):
+            self.lcd.displayString("*", self.currentEntry+2, 11)
+            self.isSet[self.currentEntry] = True
+        else:
+            self.lcd.displayString(" ", self.currentEntry+2, 11)
+            self.isSet[self.currentEntry] = False
     def onBack(self):
         pass
