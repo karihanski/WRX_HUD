ccb12ed932d5701bd144482f88deda6c2b04b9cf
Adam <adamyoung600@gmail.com>
2014-11-05 23:32:40 -0500

    Switched the menu system test case to use pygame for keyboard input instead of tkinter which wasn't working.

--- TestMenuSystem.py
+++ TestMenuSystem.py
@@ -1,16 +1,25 @@
 from MenuSystem.MenuManager import MenuManager
 from Hardware.SH1106 import SH1106LCD
-from Tkinter import *
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Python Caption')
+pygame.mouse.set_visible(0)
+
 
 lcd = SH1106LCD
 menuSystem = MenuManager(lcd)
 
-root = Tk()
-
-
-root.bind("<Up>", MenuManager.upButtonCallback)
-root.bind("<Down>", MenuManager.downButtonCallback)
-root.bind("<Return>", MenuManager.setButtonCallback)
-root.bind("<Shift>", MenuManager.backButtonCallback)
-
-root.mainloop()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_UP:
+                MenuManager.upButtonCallback()
+            if event.key == K_DOWN:
+                MenuManager.downButtonCallback()
+            if event.key == K_RETURN:
+                MenuManager.setButtonCallback()
+            if event.key == K_RSHIFT:
+                MenuManager.backButtonCallback()
